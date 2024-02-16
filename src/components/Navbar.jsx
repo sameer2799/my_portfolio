ff5df7703fdf5ea36d441ad1395286941b52789a
Sameer Singh <sameersingh2799@gmail.com>
2024-02-17 00:49:52 +0530

    Navbar edits and logo img

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, {useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+
+const Navbar = () => {
+  const [active, setActive] = useState("");
+
+  return (
+    <nav className={`
+      ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      >
+      <div className="w-full flex items-center justify-between max-w-7xL mx-auto">
+        
+        <Link to="/" 
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("")
+            window.scrollTo(0, 0);
+            }}>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+
+          <p className="text-white text-[18px] font-italic cursor-pointer">
+            Sameer <span className='sm:block hidden'>| Singh</span>
+          </p>
+          
+        </Link>
+        <p className="text-red-500">Bye</p>
+      </div>
+    
+    </nav>
+  )
+}
+
+export default Navbar
